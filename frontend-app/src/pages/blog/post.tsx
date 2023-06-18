2f2f32d2b341193754c3f022993e7d59eb6a0aec
unchain1ed <unchain1ed@outlook.jp>
2023-06-18 19:32:51 +0900

    Add Blog Post View
    Add Blog Post View

--- frontend-app/src/pages/blog/post.tsx
+++ frontend-app/src/pages/blog/post.tsx
@@ -1,9 +1,15 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import * as Yup from "yup";
+import { Layout as BlogLayout } from "src/layouts/blog/layout";
 
-const BlogForm: React.FC = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+import { Box, Button, Grid, Stack, TextField } from "@mui/material";
+import { useFormik } from "formik";
+
+const Page: React.FC = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const router = useRouter();
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -16,35 +22,101 @@ const BlogForm: React.FC = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    
+
     // ブログ投稿の処理
     // APIリクエストなどが含まれます
 
     // ブログ投稿後にブログ一覧ページにリダイレクト
-    router.push('/blogs');
+    router.push("/blogs");
   };
 
+  const formik = useFormik({
+    initialValues: {
+      // userId: "root",
+      // password: "root",
+      // submit: null,
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().max(30).required("タイトルを入力してください"),
+      content: Yup.string().max(20).required("記事内容を入力してください"),
+    }),
+    onSubmit: async (values, helpers) => {},
+  });
+
   return (
-    <div>
-      <h1>Create a New Blog</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input type="text" id="title" value={title} onChange={handleTitleChange} />
-        </div>
-        <div>
-          <label htmlFor="content">Content:</label>
-          <textarea id="content" value={content} onChange={handleContentChange} />
-        </div>
-        <div>
-          <h2>Preview:</h2>
-          <h3>{title}</h3>
-          <p>{content}</p>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          py: 8,
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ width: "100%" }}>
+              <h1>Create</h1>
+              <form onSubmit={handleSubmit}>
+                <Stack spacing={2}>
+                  <TextField
+                    error={!!(formik.touched.title && formik.errors.title)}
+                    helperText={formik.touched.title && formik.errors.title}
+                    id="title"
+                    label="Title"
+                    onBlur={formik.handleBlur}
+                    value={title && formik.values.title}
+                    onChange={(event) => {
+                      formik.handleChange(event);
+                      setTitle(event.target.value);
+                    }}
+                    fullWidth
+                  />
+                  <TextField
+                    error={!!(formik.touched.content && formik.errors.content)}
+                    helperText={formik.touched.content && formik.errors.content}
+                    id="content"
+                    label="Content"
+                    onBlur={formik.handleBlur}
+                    value={content && formik.values.content}
+                    onChange={(event) => {
+                      formik.handleChange(event);
+                      setContent(event.target.value);
+                    }}
+                    multiline
+                    rows={15}
+                    fullWidth
+                  />
+                </Stack>
+                <Button type="submit" variant="contained">
+                  Submit
+                </Button>
+              </form>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                width: "100%",
+                backgroundColor: "#f5f5f5",
+                padding: "16px",
+                borderRadius: "4px",
+              }}
+            >
+              <h2>Preview</h2>
+              <pre style={{ marginTop: "8px", whiteSpace: "pre-wrap" }}>{title}</pre>
+              <pre style={{ whiteSpace: "pre-wrap" }}>{content}</pre>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 };
 
-export default BlogForm;
+// Page.getLayout = (page) => <BlogLayout>{page}</BlogLayout>;
+
+export default Page;
